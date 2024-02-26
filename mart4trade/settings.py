bc7b99a9b9b6d8344c1eaf5ac68c7aa62b0ffe17
Shailesh Kumar <137614351+shaileshgola1987@users.noreply.github.com>
2024-02-26 17:59:09 +0530

    Update settings.py

--- mart4trade/settings.py
+++ mart4trade/settings.py
@@ -71,7 +71,7 @@ DATABASES = {
         'NAME': 'shivmarb_crm',
         'USER': 'shivmarb_crmuser',
         'PASSWORD': 'rahul123!@#',
-        'HOST': '127.0.0.1',  # Set to your PostgreSQL server's address if not localhost
+        'HOST': '89.116.121.141',  # Set to your PostgreSQL server's address if not localhost
         'PORT': '5432',       # Default PostgreSQL port
     }
 }
